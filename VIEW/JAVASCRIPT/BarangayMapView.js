87def271655055fc2b6336e236ac37fc0d2bb127
Franz-Angeles <aujscangelesfranz@gmail.com>
2025-08-07 01:11:08 +0800

    v1.14.5 - file management cleaning

---
+++ VIEW/JAVASCRIPT/BarangayMapView.js
@@ -0,0 +1,6 @@
+// Barangay Map View JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Barangay Map View loaded");
+
+  // Any specific functionality for the map view would go here
+});
